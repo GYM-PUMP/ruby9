4acde2e37c6b51471af4df12b61034d2edf0cc2b
佐々木　大器 <DD@yamadamanabunoMacBook-Pro.local>
2020-01-07 14:07:14 +0900

    [finish] 2-9

---
+++ fizz_buzz.rb
@@ -0,0 +1,69 @@
+# puts ohohoh
+# def ohohoh
+# 	"bibit"
+# end
+
+# def greeting(name)
+# 	"hello,#{name}!"
+# end
+# puts greeting(:john)
+
+# def greeting
+# 	"Hello,john!"
+# 	"Hi,john!"
+# end
+# puts greeting
+
+# def greeting
+# 	return "Hello,john!"
+# 	 "Hi,john!"
+# end
+
+# puts greeting
+
+
+# puts 10 + "1".to_i
+
+# puts 10.to_s + "1"
+
+# puts 1 == 2
+# puts 1==1
+
+# puts 8%3
+# puts 5%5
+
+# def fizz_buzz(number)
+# if number % 15 == 0
+# 	"FizzBuzz"
+# elsif number % 5 == 0
+# 	"Buzz"
+# elsif number % 3 == 0
+# 	"Fizz"
+# else
+# 	number.to_s
+# end
+# end
+
+# puts "1 以上の数字を入力してください"
+
+# input = gets.to_i
+
+# puts "結果は、、、"
+
+# puts fizz_buzz(input)
+
+
+
+# for d in 1..100
+
+# 	if d%15==0
+# 		puts "FizzBuzz"
+# 	elsif d%3==0
+# 		puts "Fizz"
+# 	elsif d%5==0
+# 		puts "Buzz"
+# 	else
+# 		puts d.to_s
+# end
+# end
+
